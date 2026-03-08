88b0d866e60feab61c70cbf37b6bc29b50df56f5
Nicco Cahya <niccocahya883@gmail.com>
2026-03-08 17:01:57 +0700

    project instalation

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full bg-purple-800 min-h-screen absolute flex flex-col">
+      
+    </div>
+  );
+}
