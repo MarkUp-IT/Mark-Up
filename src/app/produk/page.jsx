e54e036428eb79a6c7dd64edad5b9b516f4ef554
Nicco Cahya <niccocahya883@gmail.com>
2026-03-10 15:10:13 +0700

    component/navbar

--- src/app/produk/page.jsx
+++ src/app/produk/page.jsx
@@ -3,7 +3,7 @@
 import Navbar from "@/components/navbar";
 import Image from "next/image";
 
-export default function HomePage() {
+export default function ProdukPage() {
   return (
     <div className="w-full bg-gradient-to-b from-black to-purple-800 min-h-screen relative flex flex-col">
       <div className="absolute z-10 flex w-full justify-center mt-4">
